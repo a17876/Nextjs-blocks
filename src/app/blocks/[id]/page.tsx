997c08e220d8461d923d5b426fca19f9c9768c02
a17876 <a17876@hanmail.net>
2023-11-29 18:26:41 -0800

    datail page update

--- src/app/blocks/[id]/page.tsx
+++ src/app/blocks/[id]/page.tsx
@@ -1,45 +1,14 @@
-import { GetServerSideProps } from "next";
+import { db } from "@/db";
+import Link from "next/link";
 
-interface BlockDetailProps {
-  block: {
-    id: number;
-    title: string;
-    code: string;
-  };
+interface BlockShowPageProps {
+  params: { id: string };
 }
 
-const BlockDetailPage: React.FC<BlockDetailProps> = ({ block }) => {
-  if (!block) {
-    return <div>Loading...</div>;
-  }
-
-  const handleEditClick = () => {
-    window.location.href = `/blocks/${block.id}/edit`;
-  };
-
-  return (
-    <div>
-      <h1>{block.title}</h1>
-      <pre>{block.code}</pre>
-      <button onClick={handleEditClick}>Edit</button>
-    </div>
-  );
-};
-
-export const getServerSideProps: GetServerSideProps<BlockDetailProps> = async ({ params }) => {
-  const blockId = Number(params!.id);
-  // Fetch data from your database or any other source
-  const block = { id: blockId, title: "Sample Title", code: "Sample Code" };
-
-  if (!block) {
-    return {
-      notFound: true,
-    };
-  }
-
-  return {
-    props: { block },
-  };
-};
-
-export default BlockDetailPage;
+export default async function CodeDetail(props: BlockShowPageProps) {
+  const block = await db.snippet.findFirst({
+    where: { id: parseInt(props.params.id) },
+  });
+  return <pre>{block?.code}</pre>
+  // return <pre>{JSON.stringify(props)}</pre>; 
+}
